7efa620f75634e85ebcfe2f5dc1eaea4bdc2ec21
Rodion <rodionchik17@gmail.com>
2021-09-17 21:04:45 +0300

    add new example of using logger without its create

---
+++ src/main/java/JUL/LogTest.java
@@ -0,0 +1,16 @@
+package JUL;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LogTest {
+
+    private static final String CLASS_NAME = LogTest.class.getName();
+    private static final Logger LOGGER = JULStart.LOGGER;
+
+    public static void myTest () {
+        final String methodName = "myTest";
+        LOGGER.logp(Level.INFO, CLASS_NAME, methodName, "");
+        System.out.println("Using logger without its create.");
+    }
+}
