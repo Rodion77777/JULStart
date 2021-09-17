3062da6aa082f725d23b88bcf653ba9609065abc
Rodion <rodionchik17@gmail.com>
2021-09-17 21:03:48 +0300

    add new example of using logger without its create

--- src/main/java/JUL/Main.java
+++ src/main/java/JUL/Main.java
@@ -2,8 +2,8 @@ package JUL;
 
 public class Main {
     public static void main (String[] args) {
-        //JULStart.example1();
-        //JUL2Start.example2();
+        JULStart.example1();
+        JUL2Start.example2();
         LogTest.myTest();
     }
 }
